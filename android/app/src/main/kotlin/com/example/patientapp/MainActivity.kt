5563e8b7c9d3c1f170ebb06af34797a641af8d34
krisuv16 <krisuv16@gmail.com>
2023-03-04 23:30:50 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/patientapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.patientapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:patientapp/home/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
